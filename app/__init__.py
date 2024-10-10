fba4a567a198f7c97a224ab9074ae6a40434feef
luis llamas <luis.llamas@maslla.com>
2024-10-10 16:16:08 -0500

    commit3

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,6 @@ def index():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))  # Default to 8080 if PORT is not set
+    port = int(os.environ.get('PORT', 8000))  # Default to 8080 if PORT is not set
     app.run(host='0.0.0.0', port=port)
 
